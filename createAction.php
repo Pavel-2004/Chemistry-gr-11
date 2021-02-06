c9b5d0b4c9bbfe4b0b663dae9103b78bb6a0e2f3
Pavel Halko <65878619+Pavel-2004@users.noreply.github.com>
2021-02-06 02:26:45 -0500

    Delete createAction.php

--- createAction.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'functions.php';
-$student = $_GET['student'];
-$grade = $_GET['grade'];
-
-$newStudent = new create($conn, $student, $grade);
-$newStudent->Main(); 
-header("location: index.html");
