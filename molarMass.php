fb67fbcd1a490879b8371a5592334edd8d4f808e
Pavel Halko <65878619+Pavel-2004@users.noreply.github.com>
2021-02-11 08:18:32 -0500

    Add files via upload

--- molarMass.php
+++ molarMass.php
@@ -1,28 +1,73 @@
 <?php
-require('functions.php'); 
+require('functions.php');
+session_start();  
 ?>
-<h1>Molar Mass</h1>
-<p>
-molar mass: The mass of a given substance (chemical element or chemical compound in g) divided by its amount of substance (mol). mole: The amount of substance of a system that contains as many elementary entities as there are atoms in 12 g of carbon-12.
-</p>
-<h3>Formula</h3>
-<h5>Molar mass = subscript * atomic mass of element</h5>
-<h3>Calculator</h3>
-<form action="finalmolarMass.php" method="get">
-    <h5>Enter chemical formula</h5>
-    <input type="text" name="formula">
-    <input type="submit" value="submit">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="resources/molarMass.css">
+</head>
+<body>
+    <div class="container">
+        <div class="bar">
+            <div class="title">
+                <h1>Grade 11 Chemistry</h1>
+            </div>
+            <div class="sites">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Place holder</a></li>
+                    <li><a href="#">Place holder</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="left">
+            <div class="section">
+                <h1>Molar Mass calculator</h1>
+            </div>
+            <div class="description">
+                <h3>What is the Molar Mass?</h3>
+                <p>
+                The mass of a given substance (chemical element or chemical compound in g) divided by its amount of substance (mol). mole: The amount of substance of a system that contains as many elementary entities as there are atoms in 12 g of carbon-12.
+                </p>
+            </div>
+            <div class="example">
+                <div class="formula">
+                    <h1>Formula</h1>
+                    <h4>Atomic mass = atomic mass × subscript</h4>
+                </div>
+                <div class="atomicMass">
+                    <img src="resources/pictures/AtomicMas.png">
+                </div>
+                <div class="subscript">
+                    <img src="resources/pictures/subscript.png">
+                </div>
+            </div>
+            <div class="calculator">
+                <form action="finalmolarMass.php">
+                    <h5>Enter Chemical Formula</h5>
+                    <input type="text" name="formula" class="text">
+                    <input type="submit" value="calculate" class="submit">
+                </form>
+            </div>
+        </div>
+        <div class="right">
+            <div class="comments">
+                <?php
+                $showComments = new Comments($conn, '');
+                if(isset($_SESSION['login'])){
+                    $showComments->readCommentAdmin();
+                } else{
+                    $showComments->readComment();
+                } 
+                ?>
+            </div>
+        </div>
+    </div>
 
-<div style="border-style: solid;">
-    <h5>Comments</h5>
-    <?php
-        $showComments = new Comments($conn, '');
-        $showComments->readComment();
-    ?>
-    <h5>Comment</h5>
-    <form action="FinalizeComment.php" method="get">
-        <textarea name="comment" cols="30" rows="10"></textarea>
-        <input type="submit" value="submit">
-    </form>
-</div>
+</body>
+</html>
